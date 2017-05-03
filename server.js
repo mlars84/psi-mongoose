1cfd90b515534fa11c7b6c69a0cef400a74f4b21
Matt Larson <matt.a.larson@gmail.com>
2017-05-03 15:46:32 -0500

    post created

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./models/user');
+
 var app = express();
+
+app.use(bodyParser.json()); // need urlencoded for jquery/json
+
+// connection string: mongodb local, default port, name of our database
+mongoose.connect('mongodb://localhost:27017/psiUserDb');
+
+app.post('/user', function (req, res) {
+  console.log('in users post ->', req.body);
+
+  var newUser = new User({
+    name: req.body.name
+  });
+
+  res.sendStatus(200);
+});
 
 app.listen(3004, function () {
   console.log('listening on 3004');
